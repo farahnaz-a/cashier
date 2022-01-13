aee7f54147b67c8c68e08197c6c7139e7fa6f1c9
farah <ahmedfarahnaz1@gmail.com>
2022-01-13 17:48:55 +0600

    cash

---
+++ app/Http/Controllers/SubscriptionController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Cashier\Cashier;
+use \Stripe\Stripe;
+
+class SubscriptionController extends Controller
+{
+  
+    public function retrievePlans() {
+        $key = \config('services.stripe.secret');
+        $stripe = new \Stripe\StripeClient($key);
+        $plansraw = $stripe->plans->all();
+        $plans = $plansraw->data;
+        
+        foreach($plans as $plan) {
+            $prod = $stripe->products->retrieve(
+                $plan->product,[]
+            );
+            $plan->product = $prod;
+        }
+        return $plans;
+    }
+    public function index()
+    {
+        $key = \config('services.stripe.secret');
+        $stripe = new \Stripe\StripeClient($key);
+        $plansraw = $stripe->plans->all();
+        $plans = $plansraw->data;
+        
+        foreach($plans as $plan) {
+            $prod = $stripe->products->retrieve(
+                $plan->product,[]
+            );
+            $plan->product = $prod;
+        }
+        return view('subscription.create', compact('plans'));
+    }
+
+    public function orderPost(Request $request)
+    {
+            $user = auth()->user();
+            $input = $request->all();
+            $token =  $request->stripeToken;
+            $paymentMethod = $request->paymentMethod;
+            try {
+
+                Stripe::setApiKey(env('STRIPE_SECRET'));
+                
+                if (is_null($user->stripe_id)) {
+                    $stripeCustomer = $user->createAsStripeCustomer();
+                }
+
+                \Stripe\Customer::createSource(
+                    $user->stripe_id,
+                    ['source' => $token]
+                );
+
+                $user->newSubscription($request->plane,$input['plane'])
+                    ->create($paymentMethod, [
+                    'email' => $user->email,
+                ]);
+
+                return back()->with('success','Subscription is completed.');
+            } catch (Exception $e) {
+                return back()->with('success',$e->getMessage());
+            }
+            
+    }
+
+    public function webhook(Request $request)
+    {
+        return "hey";
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\SubscriptionController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::get('/subscription/create', [SubscriptionController::class, 'index'])->name('subscription.create');
+Route::post('order-post', [SubscriptionController::class, 'orderPost'])->name('order-post');
+Route::post('/stripe/webhook', [SubscriptionController::class, 'webhook'])->name('webhook');
+
+// Route::get('/seller/subscribe', [SubscriptionController::class, 'showSubscription']);
+// Route::post('/seller/subscribe', [SubscriptionController::class, 'processSubscription']);
+//       // welcome page only for subscribed users
+// Route::get('/welcome', [SubscriptionController::class, 'showWelcome'])->middleware('subscribed');
